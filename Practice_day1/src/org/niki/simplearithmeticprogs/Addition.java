bc29d722031054f00f6457a08fe29b07513e344c
nikiofficial <nikhila5nayaka@gmail.com>
2020-06-08 11:55:06 +0530

    added addition program

---
+++ Practice_day1/src/org/niki/simplearithmeticprogs/Addition.java
@@ -0,0 +1,11 @@
+package org.niki.simplearithmeticprogs;
+
+public class Addition {
+	public static void add(int num1, int num2) {
+		System.out.println("The sum of two numbers is " + (num1 + num2));
+	}
+
+	public static void main(String[] args) {
+		add(4, 5);
+	}
+}
