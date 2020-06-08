40395adf4eaeb01175772d31d1e6e63c4f07f189
nikiofficial <nikhila5nayaka@gmail.com>
2020-06-08 12:14:22 +0530

    Improvised the addition program

--- Practice_day1/src/org/niki/simplearithmeticprogs/Addition.java
+++ Practice_day1/src/org/niki/simplearithmeticprogs/Addition.java
@@ -1,11 +1,15 @@
 package org.niki.simplearithmeticprogs;
 
 public class Addition {
-	public static void add(int num1, int num2) {
-		System.out.println("The sum of two numbers is " + (num1 + num2));
+	public static void add(int... numbers) {
+		int sum = 0;
+		for(int num:numbers) {
+			sum += num;
+		}
+		System.out.println("The sum of two numbers is " + sum);
 	}
 
 	public static void main(String[] args) {
-		add(4, 5);
+		add(4, 5, 4 , 3, 2, 1, 6, 7, 2);
 	}
 }
